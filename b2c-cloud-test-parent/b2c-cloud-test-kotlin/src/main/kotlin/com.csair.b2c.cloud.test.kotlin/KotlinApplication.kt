1f6719ebaece486d97b4be7eb5fa11df4a9cc6a3
liangyudong <liangyudong@bluemoon.com.cn>
2020-06-18 17:36:16 +0800

    完善kotlin的springboot练习
    Signed-off-by: liangyudong <liangyudong@bluemoon.com.cn>

--- b2c-cloud-test-parent/b2c-cloud-test-kotlin/src/main/kotlin/com.csair.b2c.cloud.test.kotlin/KotlinApplication.kt
+++ b2c-cloud-test-parent/b2c-cloud-test-kotlin/src/main/kotlin/com.csair.b2c.cloud.test.kotlin/KotlinApplication.kt
@@ -2,11 +2,14 @@ package com.csair.b2c.cloud.test.kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
 
+@ServletComponentScan
 @SpringBootApplication
 class KotlinApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinApplication>(*args)
+    println("http://localhost:8808/hello")
 }
