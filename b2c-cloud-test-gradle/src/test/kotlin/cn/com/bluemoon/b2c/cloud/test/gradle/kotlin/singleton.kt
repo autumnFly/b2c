fabd1247e56410dbc1f844a5c5ce7382d7804e29
jufeng98 <375709770@qq.com>
2020-07-08 10:25:56 +0800

    完善kotlin练习
    Signed-off-by: liangyudong <liangyudong@bluemoon.com.cn>

---
+++ b2c-cloud-test-gradle/src/test/kotlin/cn/com/bluemoon/b2c/cloud/test/gradle/kotlin/singleton.kt
@@ -0,0 +1,16 @@
+package cn.com.bluemoon.b2c.cloud.test.gradle.kotlin
+
+/**
+ *
+ * @author yudong
+ * @date 2020/6/23
+ */
+object Resource {
+    val name = "Name"
+    // 编译期常量
+    const val NAME = "Name"
+}
+
+fun singleton() {
+    println(Resource.name)
+}
