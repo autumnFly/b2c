55ae9c2d3fe67c1ddbacdaa4d35874c803c22672
liangyudong <liangyudong@bluemoon.com.cn>
2020-07-30 20:32:59 +0800

    优化练习代码
    Signed-off-by: liangyudong <liangyudong@bluemoon.com.cn>

--- b2c-cloud-test-parent/b2c-cloud-test-boot/src/main/java/com/csair/com/cloud/test/boot/BootApplication.java
+++ b2c-cloud-test-parent/b2c-cloud-test-boot/src/main/java/com/csair/com/cloud/test/boot/BootApplication.java
@@ -4,31 +4,22 @@ import com.csair.b2c.cloud.test.common.constant.ProjectConst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
-@ImportResource(locations = { "classpath*:spring/b2c-servlet.xml" })
+@ImportResource(locations = {"classpath*:spring/b2c-servlet.xml"})
 @EnableDiscoveryClient
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = ProjectConst.REDIS_SESSION_EXPIRED_TIME)
-@ComponentScan(basePackages = {"com.csair.b2c","com.csair.com"})
-@SpringBootApplication
+@ComponentScan(basePackages = {"com.csair.b2c", "com.csair.com"})
+@SpringBootApplication(exclude = EurekaClientAutoConfiguration.class)
 public class BootApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(BootApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
 
-	}
-
-	@Bean
-	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/jsp/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
+    }
 
 }
