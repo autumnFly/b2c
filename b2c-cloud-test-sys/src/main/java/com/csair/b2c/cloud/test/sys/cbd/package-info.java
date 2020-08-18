3d9f9e05bf4d9033c1e5c77425cb1cab15183837
jufeng98 <375709770@qq.com>
2020-08-18 16:16:58 +0800

    重新完善rmi,jersey练习代码
    Signed-off-by: liangyudong <liangyudong@bluemoon.com.cn>

--- b2c-cloud-test-sys/src/main/java/com/csair/b2c/cloud/test/sys/cbd/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://web.service.newccs.cbd.csair.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.csair.b2c.cloud.test.sys.cbd;
--- b2c-cloud-test-sys/sys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export JRE_HOME=${JAVA_HOME}/jre
-export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
-export PATH=${JAVA_HOME}/bin:$PATH
-
-ps -ef | grep b2c-cloud-test-sys | grep -v grep | awk '{print $2}' | xargs kill -9
-
-nohup /usr/lib/jvm/java-7-oracle/bin/java -Xms128m -Xmx128m -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 b2c-cloud-test-sys-2.0.0-SNAPSHOT.jar >sys.log 2>&1 &
