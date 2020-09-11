8fe8488eae9f0e73890f21fb95a55ca2b5f0ddb3
liangyudong <liangyudong@bluemoon.com.cn>
2020-09-11 15:40:03 +0800

    完善jvm练习
    Signed-off-by: liangyudong <liangyudong@bluemoon.com.cn>

--- b2c-cloud-test-learn-java/src/main/java/com/csair/b2c/cloud/test/learn/java/thinking/SwingProgramTest.java
+++ b2c-cloud-test-learn-java/src/main/java/com/csair/b2c/cloud/test/learn/java/thinking/SwingProgramTest.java
@@ -2,8 +2,6 @@ package com.csair.b2c.cloud.test.learn.java.thinking;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -11,13 +9,13 @@ import java.awt.event.MouseEvent;
  * @author yu
  */
 public class SwingProgramTest {
-	static JLabel label = new JLabel("hello world");
+    static JLabel label = new JLabel("hello world");
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(() -> {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
             final JFrame frame = new JFrame();
             frame.setSize(300, 300);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             label.addMouseListener(new MouseAdapter() {
 
                 @Override
@@ -39,5 +37,5 @@ public class SwingProgramTest {
 
             }).start();
         });
-	}
+    }
 }
